e49eee9cc7cf6220693d738b21e72ff088c1f578
Kristen Schultz <kmw.schultz@gmail.com>
2017-02-15 16:52:30 -0700

    feat: App supports layout mirroring

--- app/src/main/java/com/udacity/stockhawk/ui/AddStockDialog.java
+++ app/src/main/java/com/udacity/stockhawk/ui/AddStockDialog.java
@@ -5,20 +5,28 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.udacity.stockhawk.R;
 
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 
 public class AddStockDialog extends DialogFragment {
@@ -26,6 +34,7 @@ public class AddStockDialog extends DialogFragment {
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.dialog_stock)
     EditText stock;
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -61,6 +70,9 @@ public class AddStockDialog extends DialogFragment {
         if (window != null) {
             window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         }
+
+        stock.setLayoutDirection(View.LAYOUT_DIRECTION_LOCALE);
+        stock.setTextDirection(View.TEXT_DIRECTION_LOCALE);
 
         return dialog;
     }
